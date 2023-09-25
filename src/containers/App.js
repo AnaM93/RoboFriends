15a32b3d4d6f234c6aa23cae0ab31871b2806c98
AnaM93 <ana.18_03@yahoo.com>
2023-09-25 12:28:49 -0700

    connected Robofriend App to some API and created a Scroll component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react'
-import CardList from './CardList.js'
-import {robots} from './robots.js'
-import SearchBox from './SearchBox.js'
+import CardList from '../components/CardList.js'
+import SearchBox from '../components/SearchBox.js'
+import Scroll from '../components/Scroll.js'
 import './App.css'
 
 class App extends Component {
     constructor(){
     super()
     this.state = {
-        robots : robots,
+        robots : [],
         searchField : ''
     }
+};
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(risponse => risponse.json())
+    .then(users => {this.setState({robots: users})})
 }
+
 onSearchChange = (event) =>{
     this.setState({searchField : event.target.value})
     
@@ -20,14 +26,17 @@ onSearchChange = (event) =>{
 render (){
     const filteredRobots = this.state.robots.filter(robot =>{
         return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())})
-        
+     if (this.state.robots.length === 0) {
+            return <h1> Loading...</h1>
+        } else {    
     return(
     <div className='tc'>
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-
+        <Scroll>
+             <CardList robots={filteredRobots} />
+        </Scroll>
     </div>
-)}}
+)}}}
 
 export default App;
